9a7c94325ceff0220ffc4cb34f74a9f0d756f4dc
Will <doggy.huang@gmail.com>
2018-08-23 03:13:55 +0800

    完整 Angular 基礎應用程式開發，且可以透過 Proxy 呼叫 Web API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo1';
+  imgUrl = '/assets/Spinner-1s-200px.svg';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.load_image();
+  }
+
+  load_image() {
+    this.httpClient.get('/api/breeds/image/random').subscribe((v: any) => {
+      this.imgUrl = v.message;
+    });
+  }
 }
